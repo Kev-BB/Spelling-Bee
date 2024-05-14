6762023a3ed940da880cb0e3b0c992d28ec80894
Kev-BB <118001660+Kev-BB@users.noreply.github.com>
2024-05-14 20:19:34 +0300

    Add hive of letters & input textbox

--- src/App.js
+++ src/App.js
@@ -1,9 +1,81 @@
-import React from 'react'
-
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <input typeof="text" className="text-input" placeholder="Type or Click" />
+      <div className="hive">
+        <svg className="hive-cell center" viewBox="0 0 120 104">
+          <polygon
+            className="cell-fill"
+            points="0,52 30,0 90,0 120,52 90,104 30,104"
+            strokeWidth="7.5"
+          />
+          <text className="cell-letter" x="50%" y="50%" dy="10.75%">
+            A
+          </text>
+        </svg>
+        <svg className="hive-cell outer" viewBox="0 0 120 104">
+          <polygon
+            className="cell-fill"
+            points="0,52 30,0 90,0 120,52 90,104 30,104"
+            strokeWidth="7.5"
+          />
+          <text className="cell-letter" x="50%" y="50%" dy="10.75%">
+            B
+          </text>
+        </svg>
+        <svg className="hive-cell center" viewBox="0 0 120 104">
+          <polygon
+            className="cell-fill"
+            points="0,52 30,0 90,0 120,52 90,104 30,104"
+            strokeWidth="7.5"
+          />
+          <text className="cell-letter" x="50%" y="50%" dy="10.75%">
+            C
+          </text>
+        </svg>
+        <svg className="hive-cell center" viewBox="0 0 120 104">
+          <polygon
+            className="cell-fill"
+            points="0,52 30,0 90,0 120,52 90,104 30,104"
+            strokeWidth="7.5"
+          />
+          <text className="cell-letter" x="50%" y="50%" dy="10.75%">
+            D
+          </text>
+        </svg>
+        <svg className="hive-cell center" viewBox="0 0 120 104">
+          <polygon
+            className="cell-fill"
+            points="0,52 30,0 90,0 120,52 90,104 30,104"
+            strokeWidth="7.5"
+          />
+          <text className="cell-letter" x="50%" y="50%" dy="10.75%">
+            E
+          </text>
+        </svg>
+        <svg className="hive-cell center" viewBox="0 0 120 104">
+          <polygon
+            className="cell-fill"
+            points="0,52 30,0 90,0 120,52 90,104 30,104"
+            strokeWidth="7.5"
+          />
+          <text className="cell-letter" x="50%" y="50%" dy="10.75%">
+            F
+          </text>
+        </svg>
+        <svg className="hive-cell center" viewBox="0 0 120 104">
+          <polygon
+            className="cell-fill"
+            points="0,52 30,0 90,0 120,52 90,104 30,104"
+            strokeWidth="7.5"
+          />
+          <text className="cell-letter" x="50%" y="50%" dy="10.75%">
+            G
+          </text>
+        </svg>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
