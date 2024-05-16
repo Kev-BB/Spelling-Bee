63d7ccd97769996a3209dbe04e5bbd49bcfcab2c
Kev-BB <118001660+Kev-BB@users.noreply.github.com>
2024-05-16 23:45:35 +0300

    Add functionality to buttons & Make adjustments to CSS

--- src/App.js
+++ src/App.js
@@ -1,94 +1,82 @@
 import { FaShuffle } from "react-icons/fa6";
+import { useState } from "react";
+import dictionary from "./dictionary";
 
 const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [count, setCount] = useState(0);
+  const [answers, setAnswers] = useState([]);
+
+  function handleInputChange(e) {
+    setInputValue(e.target.value);
+  }
+
+  function handleEnter(e) {
+    e.preventDefault();
+    if (dictionary.validAnswers.includes(inputValue)) {
+      setCount((count) => count + 1);
+      setInputValue("");
+      setAnswers((prevAnswers) => [...prevAnswers, inputValue]);
+    }
+  }
+
+  function handleButtonClick(letter) {
+    setInputValue(inputValue + letter);
+  }
+
+  function handleDelete() {
+    const textInput = [...inputValue];
+    textInput.splice(-1, 1);
+    setInputValue(textInput.join(""));
+  }
+
   return (
     <div className="container">
       <h1 className="title">🐝 Spelling Bee</h1>
-      <input typeof="text" className="text-input" placeholder="Type or Click" />
-      <div className="hive-container">
+      <div className="game-board">
         <div className="found-answers">
-          <p className="found-answers-count">You've found 0 answers</p>
+          <p className="found-answers-count">You've found {count} answers</p>
+          <ul className="answers-list">
+            {answers.map((answer, id = crypto.randomUUID()) => (
+              <li key={id}>{answer}</li>
+            ))}
+          </ul>
         </div>
-
-        <div className="hive">
-          <svg className="hive-cell center" viewBox="0 0 120 104">
-            <polygon
-              className="cell-fill"
-              points="0,52 30,0 90,0 120,52 90,104 30,104"
-              strokeWidth="7.5"
-            />
-            <text className="cell-letter" x="50%" y="50%" dy="10.75%">
-              A
-            </text>
-          </svg>
-          <svg className="hive-cell outer" viewBox="0 0 120 104">
-            <polygon
-              className="cell-fill"
-              points="0,52 30,0 90,0 120,52 90,104 30,104"
-              strokeWidth="7.5"
-            />
-            <text className="cell-letter" x="50%" y="50%" dy="10.75%">
-              B
-            </text>
-          </svg>
-          <svg className="hive-cell center" viewBox="0 0 120 104">
-            <polygon
-              className="cell-fill"
-              points="0,52 30,0 90,0 120,52 90,104 30,104"
-              strokeWidth="7.5"
-            />
-            <text className="cell-letter" x="50%" y="50%" dy="10.75%">
-              C
-            </text>
-          </svg>
-          <svg className="hive-cell center" viewBox="0 0 120 104">
-            <polygon
-              className="cell-fill"
-              points="0,52 30,0 90,0 120,52 90,104 30,104"
-              strokeWidth="7.5"
-            />
-            <text className="cell-letter" x="50%" y="50%" dy="10.75%">
-              D
-            </text>
-          </svg>
-          <svg className="hive-cell center" viewBox="0 0 120 104">
-            <polygon
-              className="cell-fill"
-              points="0,52 30,0 90,0 120,52 90,104 30,104"
-              strokeWidth="7.5"
-            />
-            <text className="cell-letter" x="50%" y="50%" dy="10.75%">
-              E
-            </text>
-          </svg>
-          <svg className="hive-cell center" viewBox="0 0 120 104">
-            <polygon
-              className="cell-fill"
-              points="0,52 30,0 90,0 120,52 90,104 30,104"
-              strokeWidth="7.5"
-            />
-            <text className="cell-letter" x="50%" y="50%" dy="10.75%">
-              F
-            </text>
-          </svg>
-          <svg className="hive-cell center" viewBox="0 0 120 104">
-            <polygon
-              className="cell-fill"
-              points="0,52 30,0 90,0 120,52 90,104 30,104"
-              strokeWidth="7.5"
-            />
-            <text className="cell-letter" x="50%" y="50%" dy="10.75%">
-              G
-            </text>
-          </svg>
+        <div className="hive-container">
+          <input
+            typeof="text"
+            className="text-input"
+            value={inputValue}
+            placeholder="Type or Click"
+            onChange={handleInputChange}
+          />
+          <div className="hive">
+            {dictionary.validLetters.map((letter) => (
+              <svg
+                key={letter}
+                className="hive-cell center"
+                viewBox="0 0 120 104"
+                onClick={() => handleButtonClick(letter)}
+              >
+                <polygon
+                  className="cell-fill"
+                  points="0,52 30,0 90,0 120,52 90,104 30,104"
+                  strokeWidth="7.5"
+                />
+                <text className="cell-letter" x="50%" y="50%" dy="10.75%">
+                  {letter}
+                </text>
+              </svg>
+            ))}
+          </div>
+          <div className="hive-actions">
+            <button onClick={handleDelete}>Delete</button>
+            <button>
+              <FaShuffle />
+            </button>
+            <button onClick={handleEnter}>Enter</button>
+          </div>
         </div>
-      </div>
-      <div className="hive-actions">
-        <button>Delete</button>
-        <button>
-          <FaShuffle />
-        </button>
-        <button>Enter</button>
       </div>
     </div>
   );
---
+++ src/dictionary.js
@@ -0,0 +1,8 @@
+const dictionary = 
+  {
+    validLetters: ["a", "b", "c", "d", "e", "f", "g"],
+    validPangram: ["abcdefg"],
+    validAnswers: ["bad", "bed", "cab", "deed", "dab", "face", "fade"],
+  }
+;
+export default dictionary;
